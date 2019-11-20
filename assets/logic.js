716218d0ee3e1490a2599ec3f37bd34274a15235
Daniel Taquez <daniel.taquez@du.edu>
2019-11-19 22:38:21 -0700

    api key

---
+++ assets/logic.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+// used class firebase
+    var config = {
+        apiKey: "AIzaSyCcPFcbAjIsgXGQwE-A3AcOXkeD40qypE8",
+        authDomain: "train-times-93583.firebaseapp.com",
+        databaseURL: "https://train-times-93583.firebaseio.com",
+        storageBucket: "train-times-93583.appspot.com"
+      };
+      
